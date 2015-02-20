08499c595a3cdc6e07001a451d0d1bb5f6599d84
Lawrence <li.lawrly@gmail.com>
2015-02-20 14:30:39 -0800

    imported tkinter

--- timer.py
+++ timer.py
@@ -1,18 +1,29 @@
 import time
 import sys
+from tkinter import *
+
+root = Tk()
+
+w = Label(root, text="Hello, World!")
+w.pack()
+
+root.mainloop()
+
+
+minutes = int(input('Enter number of minutes:'))
+seconds = 0
 
 time_start = time.time()
-seconds = 0
-minutes = 2
 
 while True:
+    sys.stdout.write("\r{minutes} Minutes {seconds} Seconds".format(minutes=minutes, seconds=seconds))
+    sys.stdout.flush()
+    time.sleep(0.05)
 
-    time.sleep(1)
-    if seconds == 0:
+    if minutes == 0 and seconds == 0:
+        break
+    elif seconds == 0:
         minutes -= 1
         seconds = 59
     else:
         seconds -= 1
-
-    sys.stdout.write("\r{minutes} Minutes {seconds} Seconds".format(minutes=minutes, seconds=seconds))
-    sys.stdout.flush()
