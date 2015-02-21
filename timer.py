fc3b6b4422fb5707a494a94bd3695dff432d3893
Lawrence <li.lawrly@gmail.com>
2015-02-20 16:19:26 -0800

    it's broke. will fix later

--- timer.py
+++ timer.py
@@ -2,37 +2,45 @@ import time
 import sys
 from tkinter import *
 
-minutes = 2
-seconds = 0
+
+
 
 def run_timer_label(label):
+    global minutes
     def run_timer():
-        global minutes
         global seconds
-        minutes = int(e1.get())
-
+        global minutes
+        minutes = e1.get()
         label.config(text=str("\r{minutes} Minutes {seconds} Seconds".format(minutes=minutes, seconds=seconds)))
 
-        if minutes == 0 and seconds == 0:
+        '''if minutes == 0 and seconds == 0:
             return
         elif seconds == 0:
             minutes -= 1
             seconds = 59
         else:
-            seconds -= 1
+            seconds -= 1'''
         label.after(1000, run_timer)
+
+    print(minutes)
+    minutes = e1.get()
     run_timer()
 
 root = Tk()
 root.title("Meditation Timer")
+
+minutes = IntVar()
+minutes.set(0)
+seconds = 0
+
 label = Label(root, fg="green")
 label.pack()
+
 e1 = Entry(root)
 e1.pack()
+
 b2 = Button(root, text='Start Timer', command=(run_timer_label(label)))
 b2.pack()
-button = Button(root, text='DESTROY', width=25, command=root.destroy)
-button.pack()
 
 root.mainloop()
 
